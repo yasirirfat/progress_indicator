09900c959c45588c3509684f8fbe5d23637f0ebd
FIREFLY LAPTOP'S <anabiyafatima0518@gmail.com>
2026-08-13 02:19:43 +0500

    progress indicator painter

--- lib/download_progress_view.dart
+++ lib/download_progress_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:progress_indicator/loading_indicator.dart';
+import 'package:progress_indicator/loading_indicator_painter.dart';
 
 class DownloadProgressView extends StatelessWidget {
   final double endAngle;
